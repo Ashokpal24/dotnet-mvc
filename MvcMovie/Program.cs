614047be7678341820ecca2cce6d220adb2f59e2
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-12-04 15:15:19 +0000

    Implemented seeding of database and migrated models to create db.

--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
+using MvcMovie.Models;
 var builder = WebApplication.CreateBuilder(args);
 if (builder.Environment.IsDevelopment())
 {
@@ -16,6 +17,12 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
