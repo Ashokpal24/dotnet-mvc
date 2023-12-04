4469adf6edcb45b26f35183a56ed9e6bd94fea20
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-12-04 13:25:47 +0000

    Implemented views and tried remove boilerplate code.

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -6,10 +6,7 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/
-        public string Index()
-        {
-            return "This is my default action...";
-        }
+        public IActionResult Index() { return View(); }
 
         // GET: /HelloWorld/Welcome
         public string Welcome(string name, int ID = 1)
